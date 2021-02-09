143ca95bc7ccebd9d287e13ab2cab0139a7f852b
Kristofer <kristofervool@gmail.com>
2021-02-09 10:20:52 +0200

    Panga front ja back.

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import Register from '../views/Register.vue'
 import Bank from '../views/Bank.vue'
 import Transactionhistory from "@/views/Transactionhistory";
 Vue.use(VueRouter)
@@ -9,27 +7,6 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/About.vue')
-    }
-  },
-  {
-    path: '/register',
-    name: 'Register',
-    component: Register
-
-  },
-  {
-    path: '/Bank',
     name: 'Bank',
     component: Bank
   },
